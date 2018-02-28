561c119943690cec26fe1b275363c795873a0400
Jarosław Terejko <j.terejko@gmail.com>
2018-02-28 21:45:11 +0100

    added some fixes

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,6 @@ openMenu.addEventListener('click', openNav);
 
 closeMenu.addEventListener('click', closeNav);
 
-//showMoreBtn.addEventListener('click', showMore);
-
-//showMoreBtn.addEventListener('click', showLess);
-
 
 function openNav() {
     overlay.style.width = "100%";
@@ -29,62 +25,24 @@ function openNav() {
 }
 
 function closeNav() {
-    overlay.style.width = '0%';
+    overlay.style.width = '0';
 }
 
 
-// dzialajaca funkcja  !!!!!!!!!!!!!!!!!!!!!  
-//function showMore(e) {
-//    e.preventDefault();
-//    if (showGallery[0].style.display !== "grid") {
-//        showGallery[0].style.display = 'grid';
-//        showMoreBtn.innerHTML = 'show less';
-//    } else {
-//        showGallery[0].style.display = 'none';
-//        showMoreBtn.innerHTML = 'show more';
-//
-//    }
-//}
-
 $('.gallery-hide-show').css('display', 'none');
-
-
-//$('#show-more-btn').click(function (e) {
-//    e.preventDefault();
-////    $('.gallery-hide-show').css('display', 'grid');
-////    $('.gallery-hide-show').slideToggle(1000);
-//    if (showMoreBtn.innerHTML === 'show more') {
-//        
-//        showMoreBtn.innerHTML = 'show less';
-//        
-//        $('.gallery-hide-show').slideDown(1000);
-//    } else {
-//            showMoreBtn.innerHTML = 'show more';
-//        $('.gallery-hide-show').slideUp(1000);
-//        }
-//});
 
 $('#show-more-btn').click(function (e) {
     e.preventDefault();
-//    $('.gallery-hide-show').css('display', 'grid');
     $('.gallery-hide-show').slideToggle(1000);
-    
     if ($(showMoreBtn).text() !== 'show less') {
-        
         $(showMoreBtn).text('show less');
-        
-        //$('.gallery-hide-show').slideDown(1000);
     } else {
             $(showMoreBtn).text('show more');
-        //$('.gallery-hide-show').slideUp(1000);
         }
 });
 
 
-
-
-
-// slider
+// unslider
 $('.slider').unslider({
     autoplay: true,
     arrows: true,
